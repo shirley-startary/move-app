8326573e5e0aaf5e7f0551e72608509d8a73e3ed
Shirley Silvana Suarez Startary <Shirley Silvana Suarez Startary>
2017-06-08 15:08:11 -0500

    creando el modojuego.html y funcion al boton de jugar

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var cargarPagina = function(){
+  $("#btnjugar").click(cambiarPagina);
+}
+
+var cambiarPagina = function(e){
+  e.preventDefault();
+  location.href = "views/modojuego.html"
+}
+$(document).ready(cargarPagina);
