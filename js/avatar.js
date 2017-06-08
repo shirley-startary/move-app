34212579ba518c43690364f94c20b189cf135631
SilviaGuerra <silvia.guerrahdz@gmail.com>
2017-06-08 16:19:40 -0500

    carrusel de imagenes del avatar

--- js/avatar.js
+++ js/avatar.js
@@ -1,11 +1,41 @@
-function cargarPagina (){
-  $(".cabello-amarillo").click(editarAvatar);
-}
+var target = 0;
+var cargarPagina = function(){
+  //Tomamos  los  Elementos
+  var $botones = $(".control");
+  var $anterior = $(".previous");
+  var $siguiente = $(".next");
 
-function editarAvatar(){
-  if($(".cabello-amarillo") !== $){
-    $(".avatar").attr("src", "../assets/img/monos-12.png");
-  }
-}
+  //Eventos que se ejecutan
 
+
+  $anterior.click(anteriorImagen);
+  $siguiente.click(siguienteImagen);
+  $botones.click(cambiarImagen);
+
+};
+var cambiarImagen = function(){
+  target = parseInt ($(this).data("target")); //target=this.currentTarget
+  mostrarImagen(target);
+};
+var mostrarImagen = function(target){
+  var lastSlide = $("div.active");
+  var slide = $("div[data-slide = '" + target +"']");
+  lastSlide.removeClass("active");
+  slide.addClass("active");
+};
+var anteriorImagen = function (e) {
+  e.preventDefault();
+
+  target = target - 1;
+  target = (target < 0) ? 3 : target;
+  mostrarImagen(target);
+};
+var siguienteImagen = function (e) {
+  e.preventDefault();
+
+  target = target + 1;
+  target = (target > 3) ? 0 : target;
+  mostrarImagen(target);
+
+};
 $(document).ready(cargarPagina);
